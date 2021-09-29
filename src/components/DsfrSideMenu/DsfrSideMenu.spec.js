c134f98c1f4dcbbacf00df9cee447624a7d37c2e
Stanislas Ormières <stan@stormier.ninja>
2021-09-29 18:03:22 +0200

    WIP

---
+++ src/components/DsfrSideMenu/DsfrSideMenu.spec.js
@@ -0,0 +1,94 @@
+import { render } from '@testing-library/vue'
+
+import DsfrSideMenu from './DsfrSideMenu.vue'
+
+const VIcon = { props: ['name'], template: '<i :class="name"></i>' }
+
+describe('DsfrSideMenu', () => {
+  it('should render DsfrSideMenu with two `ul`', () => {
+    // Given
+    const headingTitle = 'Titre de rubrique I'
+    const firstLevelMenuLabel = 'Premier niveau - I.1'
+    const secondLevelMenuLabel11 = 'Deuxième niveau - I.1.1'
+    const secondLevelMenuLabel12 = 'Deuxième niveau - I.1.2'
+    const firstLevelMenuLabel2 = 'Premier niveau - I.2'
+    const secondLevelMenuLabel21 = 'Deuxième niveau - I.1.1'
+    const secondLevelMenuLabel22 = 'Deuxième niveau - I.1.2'
+
+    const headingTitle2 = 'Titre de rubrique II'
+    const firstLevelMenuLabel22 = 'Premier niveau - II.1'
+    // const secondLevelMenuLabel211 = 'Deuxième niveau - II.1.1'
+    // const secondLevelMenuLabel212 = 'Deuxième niveau - II.1.2'
+    // const firstLevelMenuLabel22 = 'Premier niveau - II.2'
+    // const secondLevelMenuLabel221 = 'Deuxième niveau - II.1.1'
+    // const secondLevelMenuLabel222 = 'Deuxième niveau - II.1.2'
+
+    const firstMenuList = {
+      title: headingTitle,
+      links: [
+        [
+          {
+
+          },
+        ],
+      ],
+    }
+
+    // When
+    const { container, getByText } = render(DsfrSideMenu, {
+      global: {
+        components: {
+          VIcon,
+        },
+      },
+      props: {
+        menu: [
+          {
+            title: headingTitle,
+            links: [
+              {
+                title: firstLevelMenuLabel,
+                links: [
+                  { to: '1.1', label: secondLevelMenuLabel11 },
+                  { to: '1.2', label: secondLevelMenuLabel12 },
+                ],
+              },
+              {
+                title: '',
+                links: [
+                  { to: '1.1', label: secondLevelMenuLabel21 },
+                  { to: '1.2', label: secondLevelMenuLabel22 },
+                ],
+              },
+            ],
+          },
+          {
+            title: headingTitle2,
+            links: [
+              {
+                title: firstLevelMenuLabel2,
+                links: [
+                  { to: '1.1', label: secondLevelMenuLabel21 },
+                  { to: '1.2', label: secondLevelMenuLabel22 },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    })
+
+    const firstHeading = getByText(headingTitle)
+    const firstLevelMenuLabel22Label = getByText(firstLevelMenuLabel22)
+    const firstLevelMenuUl = getByText(firstLevelMenuLabel)
+    const firstLevelMenu2Ul = getByText(firstLevelMenuLabel2)
+
+    // Then
+    expect(firstLevelMenuLabel22Label).toHaveClass()
+    expect(firstMenuList).toHaveClass()
+    expect(firstHeading).toHaveClass()
+    expect(firstLevelMenuUl).toHaveClass()
+    expect(firstLevelMenu2Ul).toHaveClass()
+    expect(container.querySelector('')).toHaveClass()
+  })
+})
---
+++ src/components/DsfrSideMenu/DsfrSideMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav
+    class="fr-sidemenu"
+    aria-label="Menu latéral"
+  >
+    <div class="fr-sidemenu__inner">
+      <button
+        class="fr-sidemenu__btn"
+        hidden
+        aria-controls="fr-sidemenu-wrapper"
+        aria-expanded="false"
+      >
+        Dans cette rubrique
+      </button>
+      <div
+        v-for="({ title, menus}, i) of menus"
+        id="fr-sidemenu-wrapper"
+        :key="i"
+        class="fr-collapse"
+      >
+        <div class="fr-sidemenu__title">
+          {{ title }}
+        </div>
+        <ul
+          v-for="(links, j) of menus"
+          :key="j"
+          class="fr-sidemenu__list"
+        >
+          <li
+            v-for="(link, k) of links"
+            :key="k"
+            class="fr-sidemenu__item  fr-sidemenu__item--active"
+          >
+            <a
+              v-if="typeof link.to === 'string' && link.to.startsWith('http')"
+              class="fr-sidemenu__link"
+              :href="link.to"
+              target="_self"
+              aria-current="page"
+            >{{ link.label }}</a>
+            <router-link
+              class="fr-sidemenu__link"
+              :to="link.to"
+              target="_self"
+              aria-current="page"
+            >
+              {{ link.label }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
